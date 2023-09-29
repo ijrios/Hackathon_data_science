e01fac392fc9e23cbcf18120d00a21e0294e7008
ijrios <joserios055@gmail.com>
2023-09-29 19:34:11 +0000

    Added the Web Applications.

--- app.py
+++ app.py
@@ -1,45 +1,30 @@
-from flask import Flask, request, render_template
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from flask import Flask, request, render_template
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+
 
 app = Flask(__name__)
-
-# Cargamos datos
-datos_entrenamiento = pd.read_csv('data/CVD_cleaned.csv')
-
-X = datos_entrenamiento[['edad', 'otras_caracteristicas']]  
-y = datos_entrenamiento['variable_objetivo']  
-
-# Entrenar el modelo Random Forest
-modelo_rf = RandomForestClassifier()
-modelo_rf.fit(X, y)
 
 @app.route("/")
 def hello_hackathon():
     return render_template('home.html')
 
-@app.route('/procesar_encuesta', methods=['POST'])
-def procesar_encuesta():
-    datos_encuesta = {
-        'General_Health': float(request.form['general_fealth']),
-        'Checkup': request.form['checkup'],
-        'Heart_Disease': request.form['heart_Disease'],
-        'Depression': request.form['depression'],
-        'Arthritis': request.form['arthritis'],
-        'Sex': request.form['sex'],
-    }
+def data():
+  # Cargamos datos
+  datos = pd.read_csv('data/datos_novus.csv')
+  X = datos.drop(["Heart_Disease"],axis=1)
+  y = datos['Heart_Disease']
+  scaler = MinMaxScaler()
+  scaler.fit(X)
+  X_train_scaled= scaler.transform(X)
 
-    resultado_prediccion = modelo_rf.predict([[
-        datos_encuesta['General_Health'],
-        datos_encuesta['Checkup'],
-        datos_encuesta['Heart_Disease'],
-        datos_encuesta['Depression'],
-        datos_encuesta['Arthritis'],
-        datos_encuesta['Sex']
-      
-    ]])
+  # Entrenar el modelo Random Forest
+  modelo_rf = RandomForestClassifier()
+  modelo_rf.fit(X_train_scaled, y)
 
-    return render_template('resultado.html', resultado=resultado_prediccion[0])
 
 if __name__  == "__main__":
   app.run(host="0.0.0.0", debug=True)
